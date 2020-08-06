3cdb754c4ab755db7a265200098b86db073e582c
Dilan Gecmen <dbgecmen@gmail.com>
2020-08-06 16:03:17 +0200

    Remove files

--- KNNRef.m
+++
@@ -1,6 +0,0 @@
-function [b1,b2]=refknn(k,knn)
-a = knn(:,k);
-b1 = sum(a<=60);
-b2 = sum(a>60);
-end
-
--- bagembed.m
+++
@@ -1,6 +0,0 @@
-function [mB] = bagembed(x,B,sigma)
-    mB = zeros(1,length(x));
-    for i = 1:length(x)
-        mB(i) = max(exp(-vecnorm(B-x(i,:),2,2).^2/sigma^2));
-    end
-end
